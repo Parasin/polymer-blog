008d97affa084d33e45ede4ca9be7eea8df9e171
eevabec <evan.bechtol@ericsson.com>
2017-09-23 18:19:57 -0500

    Add secureRequest method to ensure HTTPS always used

--- routes/index.js
+++ routes/index.js
@@ -14,21 +14,53 @@ let routes = ( app ) => {
 		next();
 	} );
 
-	app.use( `${config.appRoot}`, ( req, res, next ) => {
-		return next();
-	}, Base );
+	if ( process.env.ENV === 'Production' ) {
+		app.use( `${config.appRoot}`, ( req, res, next ) => {
+			return next();
+		}, Base );
 
-	app.use( `${config.appRoot}user`, ( req, res, next ) => {
-		return next();
-	}, User );
+		app.use( `${config.appRoot}user`, ( req, res, next ) => {
+			return next();
+		}, User );
 
-	app.use( `${config.appRoot}post`, ( req, res, next ) => {
-		return next();
-	}, Post );
+		app.use( `${config.appRoot}post`, ( req, res, next ) => {
+			return next();
+		}, Post );
 
-	app.use( `${config.appRoot}auth`, ( req, res, next ) => {
-		return next();
-	}, Auth );
+		app.use( `${config.appRoot}auth`, ( req, res, next ) => {
+			return next();
+		}, Auth );
+	} else {
+		app.use( `${config.appRoot}`, secureRequest, ( req, res, next ) => {
+			return next();
+		}, Base );
+
+		app.use( `${config.appRoot}user`, secureRequest, ( req, res, next ) => {
+			return next();
+		}, User );
+
+		app.use( `${config.appRoot}post`, secureRequest, ( req, res, next ) => {
+			return next();
+		}, Post );
+
+		app.use( `${config.appRoot}auth`, secureRequest, ( req, res, next ) => {
+			return next();
+		}, Auth );
+	}
 };
+
+function secureRequest( req, res, next ) {
+	if( req.headers['x-forwarded-proto'] === 'https' ) {
+		// OK, continue
+		return next( );
+	}
+
+	if ( req.baseUrl === '/metrics' ) {
+		res.redirect( 'https://' + req.headers.host + '/metrics' );
+	} else {
+		res.redirect( 'https://' + req.headers.host + '/assess' + req.path );
+	}
+
+}
 
 module.exports = routes;
